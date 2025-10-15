2b2b356eadeb6d67d6b2b2077422a86f3056f6a5
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-15 15:52:50 +0300

    working with put and get and post all with API

--- src/main/java/com/springbootapp/democrudapi/repository/UserController.java
+++ src/main/java/com/springbootapp/democrudapi/repository/UserController.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,32 @@ public class UserController {
     public List<Employee> getlist() {
         return employeeService.findAll();
 
+    }
+
+    // GET API code
+    @GetMapping("/employees/{employeeid}")
+    public Employee getMyEmployee(@PathVariable int employeeid) {
+
+        return employeeService.findById(employeeid);
+    }
+
+    // POST API
+    @PostMapping("/employees")
+    public Employee postnewEmployee(@RequestBody Employee employee) {
+        // set id to zero incase so it will create new item instead of update
+        employee.setId(0);
+
+        Employee employee1 = employeeService.save(employee);
+        return employee1;
+    }
+
+    // Put Mapping
+    @PutMapping("/employees")
+    public Employee updateemployee(@RequestBody Employee employee) {
+        // TODO: process PUT request
+        Employee myeEmployee = employeeService.save(employee);
+
+        return myeEmployee;
     }
 
 }
