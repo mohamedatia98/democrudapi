4078e66038d7b90406b1b32526ba747b38662dff
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-15 13:53:08 +0300

    Refactor the code to using Service annotation instead of Repository  on usercontroller

--- src/main/java/com/springbootapp/democrudapi/repository/UserController.java
+++ src/main/java/com/springbootapp/democrudapi/repository/UserController.java
@@ -2,28 +2,30 @@ package com.springbootapp.democrudapi.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootapp.democrudapi.entity.Employee;
+import com.springbootapp.democrudapi.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    private EmployeeDao employeeDao;
+    private EmployeeService employeeService;
 
     // inject employee ( Constructor inejction )
-
-    public UserController(EmployeeDao employeeDao) {
-        this.employeeDao = employeeDao;
+    @Autowired
+    public UserController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     // get endpoint to check the employees
     @GetMapping("/employees")
     public List<Employee> getlist() {
-        return employeeDao.findAll();
+        return employeeService.findAll();
 
     }
 
