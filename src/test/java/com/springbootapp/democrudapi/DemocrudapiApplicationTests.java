aab2098f72014316fb72c9d87f07db932612ea8b
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-15 10:26:11 +0300

    first commit

---
+++ src/test/java/com/springbootapp/democrudapi/DemocrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapp.democrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
